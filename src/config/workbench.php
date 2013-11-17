80bdb5fab93b575aec19de01d106d204862edea7
Eric Villard <dev@eviweb.fr>
2013-11-17 02:05:59 +0100

    add new configuration template
    Signed-off-by:Eric Villard <dev@eviweb.fr>

---
+++ src/config/workbench.php
@@ -0,0 +1,57 @@
+<?php
+
+return array(
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Workbench Composer Related Information
+    |--------------------------------------------------------------------------
+    |
+    | When you create new packages via the Artisan "workbench" command 
+    | a composer.json configuration is generated. Use the following to
+    | initialize it with defaults values
+    |
+    */
+    'composer' => array(
+        
+        /*
+        |----------------------------------------------------------------------
+        | Composer Authors
+        |----------------------------------------------------------------------
+        |
+        | Set here authors details : name, email, homepage, role
+        | duplicate the default array structure below to add a new author in
+        | the list of authors
+        |
+        */
+        'authors' => array(
+            
+            /*
+            |------------------------------------------------------------------
+            | Author information array
+            |------------------------------------------------------------------
+            | duplicate it to add a new author
+            |
+            */
+            array(
+                'name' => '',
+
+                'email' => '',
+
+                'homepage' => '',
+
+                'role' => '',
+            ),
+        ),
+        
+        /*
+        |----------------------------------------------------------------------
+        | License
+        |----------------------------------------------------------------------
+        |
+        | set here the license name under which your plugin will be distribute
+        |
+        */
+        'license' => '',
+    )
+);
