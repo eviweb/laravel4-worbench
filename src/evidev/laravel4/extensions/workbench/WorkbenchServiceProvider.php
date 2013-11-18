c794d0c5fcb2185667c8e6682f21125697610a90
Eric Villard <dev@eviweb.fr>
2013-11-19 00:55:39 +0100

    add WorkbenchServiceProvider
    Signed-off-by:Eric Villard <dev@eviweb.fr>

---
+++ src/evidev/laravel4/extensions/workbench/WorkbenchServiceProvider.php
@@ -0,0 +1,88 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+/**
+ * The MIT License
+ *
+ * Copyright 2013 Eric VILLARD <dev@eviweb.fr>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package     evidev\laravel4\extensions\workbench
+ * @author      Eric VILLARD <dev@eviweb.fr>
+ * @copyright   (c) 2013 Eric VILLARD <dev@eviweb.fr>
+ * @license     http://opensource.org/licenses/MIT MIT License
+ */
+
+namespace evidev\laravel4\extensions\workbench;
+
+use Illuminate\Support\ServiceProvider;
+use evidev\laravel4\extensions\workbench\PackageCreator;
+use evidev\laravel4\extensions\workbench\console\WorkbenchCommand;
+
+/**
+ * WorkbenchServiceProvider
+ *
+ * @package     evidev\laravel4\extensions\workbench
+ * @author      Eric VILLARD <dev@eviweb.fr>
+ * @copyright   (c) 2013 Eric VILLARD <dev@eviweb.fr>
+ * @license     http://opensource.org/licenses/MIT MIT License
+ */
+class WorkbenchServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['package.creator'] = $this->app->share(
+            function ($app) {
+                return new PackageCreator($app['files']);
+            }
+        );
+
+        $this->app['command.workbench'] = $this->app->share(
+            function ($app) {
+                return new WorkbenchCommand($app['package.creator']);
+            }
+        );
+
+        $this->commands('command.workbench');
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('package.creator', 'command.workbench');
+    }
+}
